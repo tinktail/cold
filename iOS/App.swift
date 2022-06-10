2ad5285fe661c245927f50eebeba949883a93757
vaux <zero.griffin@gmail.com>
2022-06-10 12:12:06 +0200

    Configure project

---
+++ iOS/App.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+@main struct App: SwiftUI.App {
+//    @Environment(\.scenePhase) private var phase
+//    @UIApplicationDelegateAdaptor(Delegate.self) private var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            Circle()
+                .task {
+//                    cloud.ready.notify(queue: .main) {
+//                        cloud.pull.send()
+//                        Defaults.start()
+//
+//                        Task
+//                            .detached {
+//                                _ = await UNUserNotificationCenter.request()
+//                                await store.launch()
+//                            }
+//                    }
+                }
+        }
+//        .onChange(of: phase) {
+//            switch $0 {
+//            case .active:
+//                cloud.pull.send()
+//            default:
+//                break
+//            }
+//        }
+    }
+}
